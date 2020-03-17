0ac520efeea3d357d02a83985a3fb576f04fdb0e
ezio <ezio3000@yahoo.it>
2020-03-17 17:30:34 +0100

    m 3

--- src/main/java/it/polito/tdp/prova1/EntryPoint.java
+++ src/main/java/it/polito/tdp/prova1/EntryPoint.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class  EntryPoint extends Application {
+public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
